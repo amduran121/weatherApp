7752c66a5babd0b891af1b1ce0d3b55b1df19337
Austin Duran <amduran121@gmail.com>
2021-11-17 18:28:44 -0700

    pushWednesday

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,41 @@
-var input = document.getElementById("value");
-var button = document.getElementById("searchBtn");
-
-function getSearchValue() {
-  var value = input.value;
-  console.log(value);
-  searchWeather(value);
+//VARIABLES
+var searchBtn = document.getElementsByClassName("searchBtn");
+var city = document.getElementsByClassName("userInput");
+var requestUrl =
+  `api.openweathermap.org/data/2.5/forecast?q=` +
+  city.value +
+  `&appid=4c4bcf68ece59c4349091a9dded4ab01`;
+getLocal();
+/* function fetchApi(city) {
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+      console.log(data);
+    })
+    .then(function (data) {
+      //api data being pulled
+    });
+} */
+//FUNCTIONS
+/* function cityInput(event) {
+  event.preventDefault();
+  var userCity = city.value;
 }
-
-
-
-function searchWeather(value) {
-//create a variable endpoint to the weather map API, that will also need to dynamically enter search value into it. i'll need towrite a fetch call the variable endpoint. "".thens" two of them and after the second one is when you dynamically create the elements. it will create cards to displya what infomration comes back from the API. once created, they wil need to be appended to an existing element in the html.
+ */
+function getLocal() {
+  var retrieve = localStorage.getItem("Cities-Searched");
+  if (retrieve === null) {
+    return [];
+  }
+  return JSON.parse(retrieve);
 }
-
-button.addEventListener("click", getSearchValue);
+//EVENT LISTENERS
+document
+  .querySelector(".searchBtn")
+  .addEventListener("click", function (event) {
+    var citySearch = city.value;
+    var previousSearches = getLocal();
+    previousSearches.push(citySearch);
+    localStorage.setItem("Cities-Searched", JSON.stringify(previousSearches));
+    fetchApi(requestUrl);
+  });
